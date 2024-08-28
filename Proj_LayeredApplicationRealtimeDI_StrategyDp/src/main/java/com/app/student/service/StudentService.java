c05149fa9cf47a2b26b9e5d703043109d4bc1069
AmitPalGithub <95398239+AmitPalGithub@users.noreply.github.com>
2024-08-28 13:42:43 +0530

    Delete Proj_LayeredApplicationRealtimeDI_StrategyDp directory

--- Proj_LayeredApplicationRealtimeDI_StrategyDp/src/main/java/com/app/student/service/StudentService.java
+++
@@ -1,8 +0,0 @@
-package com.app.student.service;
-
-import com.app.student.dto.StudentDto;
-
-public interface StudentService {
-
-	public int registerNewStudent(StudentDto dto) throws Exception;
-}
